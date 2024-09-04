ac7b019a224088f0c3fb793cdcf320108a9dd07f
gleb_papulovsky <papulovskijglech@gmail.com>
2024-09-04 16:19:26 +0300

    v1

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,17 +1,72 @@
 package ru.netology;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public abstract class Main {
+    public static void main(String[] args) {
+
+        Basket basket=new Basket();
+        Offers offers=new Offers();
+        Scanner in = new Scanner(System.in);
+
+
+        while (true) {
+            printChoicesOffers();
+            System.out.print("Input your choice: ");
+            int choice = in.nextInt();
+            switch (choice) {
+                case 1: {//Show offers
+                    System.out.println(offers);
+                    waitingAction();
+                    break;
+                }
+                case 2: {//Put offer in my basket
+                    System.out.print("Input offers index: ");
+                    basket.putItem(offers.getOfferByuIndex(in.nextInt()-1));
+
+                    waitingAction();
+                    break;
+                }
+                case 3: {//Show my basket
+                    System.out.println(basket);
+
+                    waitingAction();
+                    break;
+                }
+                case 4: {//Delete item from my basket
+                    System.out.print("Input index for deleting: ");
+                    basket.deleteItemByIndex(in.nextInt()-1);
+                    waitingAction();
+                    break;
+                }
+                case 5: {//End this session
+                    System.out.println("Goodbyyye!!!");
+                    System.exit(0);
+                    break;
+                }
+
+            }
         }
+
+
     }
+
+    public static void printChoicesOffers() {
+        System.out.println("-------Menu-------");
+        System.out.println("1. Show offers.");
+        System.out.println("2. Put offer in my basket.");
+        System.out.println("3. Show my basket.");
+        System.out.println("4. Delete item from my basket");
+        System.out.println("5. End this session.");
+        System.out.println("------------------");
+    }
+    //DRY
+    public static void waitingAction(){
+        System.out.print("Enter bottom for continue...");
+        Scanner in=new Scanner(System.in);
+        in.nextLine();
+
+    }
+
+
 }
---
+++ src/main/java/ru/netology/Offers.java
@@ -0,0 +1,43 @@
+package ru.netology;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public  class Offers {
+    private Map<String,Integer> nameAndPriceMap =new HashMap();
+
+    public Offers(){
+        this.nameAndPriceMap.put("Milk",66);
+        this.nameAndPriceMap.put("Bread",40);
+        this.nameAndPriceMap.put("Eggs",120);
+        this.nameAndPriceMap.put("Wings",999);
+        this.nameAndPriceMap.put("PotionOfSuperPower",10000);
+    }
+
+    public Map.Entry<String,Integer>getOfferByuIndex(int index){
+        if (index<0 || index+1>nameAndPriceMap.size())throw new IllegalArgumentException("Wrong index.It's cant be less then zero!");
+        int iter=0;
+        for (Map.Entry<String,Integer>entry:nameAndPriceMap.entrySet()){
+            if (index==iter)return entry;
+            iter++;
+        }
+
+        return null;
+
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder outputBuilder=new StringBuilder();
+        //magic number
+        int counter=0;
+        for (Map.Entry<String, Integer> entry : nameAndPriceMap.entrySet()) {
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+            outputBuilder.append((counter+1)+". Name: "+key+" Price: "+value+"\n");
+            counter++;
+        }
+        return outputBuilder.toString() ;
+    }
+}
